0b05b262bc9d06c79c25e89245ea789f4d7bf688
Forks & Spoons Corporation <spoons_and_forks@aol.com>
2025-08-21 07:04:46 +0530

    Update vault.js

--- js/vault.js
+++ js/vault.js
@@ -1,9 +1,9 @@
 // --- IMPORTANT: SUPABASE SETUP ---
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
 const SUPABASE_URL = 'https://cxuteylfjxbbwyvlkcew.supabase.co'; 
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4dXRleWxmanhiYnd5dmxrY2V3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU2OTAzMDIsImV4cCI6MjA3MTI2NjMwMn0.cyhbaptKZBWNzwv-NFsuVFMqr2Oo54mT9EX7c-F0S5w';
 
-const { createClient } = supabase;
 const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 // --- END OF SUPABASE SETUP ---
 
